990f9f9eadafcd845fd88b3243c30bf868780a07
veys66 <veysdoganer@gmail.com>
2022-12-10 18:00:57 +0300

    bn

---
+++ week12_11_26_2022/Test.java
@@ -0,0 +1,17 @@
+package week12_11_26_2022;
+
+public class Test {
+
+
+    public Test(){
+        System.out.println("Developer");
+    }
+    public void Test(){
+        System.out.println("SDET");
+    }
+
+    public static void main(String[] args) {
+        Test obj= new Test();
+    }
+
+}
