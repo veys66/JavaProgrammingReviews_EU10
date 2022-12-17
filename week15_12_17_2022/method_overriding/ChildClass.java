4ad51426a2466cde539f6fd0b5c9be6cf63a8798
veys66 <veysdoganer@gmail.com>
2022-12-17 19:08:10 +0300

    Week 15

---
+++ week15_12_17_2022/method_overriding/ChildClass.java
@@ -0,0 +1,14 @@
+package week15_12_17_2022.method_overriding;
+
+public class ChildClass extends ParentClass{
+
+    //Access modifier should be more visible or same
+    //Return type should be same
+    //Method name should be same
+    //Parameters should be same
+
+    @Override
+    public void move() {
+        super.move();
+    }
+}
