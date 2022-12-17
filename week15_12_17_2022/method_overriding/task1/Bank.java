f8af15c29044ce1a7f08dd371e65b822b96018e6
veys66 <veysdoganer@gmail.com>
2022-12-17 19:11:29 +0300

    Week 15

---
+++ week15_12_17_2022/method_overriding/task1/Bank.java
@@ -0,0 +1,15 @@
+package week15_12_17_2022.method_overriding.task1;
+
+public class Bank {
+
+    private int capital=10000;
+
+    public int getCapital(){
+        return capital;
+    }
+
+    public void calculateTheInterest(){
+
+    }
+
+}
