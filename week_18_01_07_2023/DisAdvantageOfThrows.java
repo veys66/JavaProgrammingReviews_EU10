e7cc606d19cabd4eeebcf11ffe9474462de9a250
veys66 <veysdoganer@gmail.com>
2023-01-07 19:13:58 +0300

    Week 18

---
+++ week_18_01_07_2023/DisAdvantageOfThrows.java
@@ -0,0 +1,34 @@
+package week_18_01_07_2023;
+
+
+
+public class DisAdvantageOfThrows {
+    public static void main(String[] args) throws InterruptedException {
+
+        //ma:c command + alt + T
+        //windows: ctrl+alt+T
+        waitFor(3);
+        sleep(4);
+    }
+    public  static void sleep(int seconds){
+        try {
+            Thread.sleep(seconds*1000);
+        } catch (InterruptedException e) {
+
+        }
+    }
+    public static void methodB(){
+        sleep(4);
+    }
+
+
+    private static void waitFor(int seconds) throws InterruptedException {
+        sleep(seconds*1000);
+    }
+
+    public static void methodA()throws InterruptedException{
+        waitFor(4);
+    }
+
+
+}
