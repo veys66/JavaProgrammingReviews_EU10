48d47fdcd7c081c84a20d8ec3f01aad75dd605f9
veys66 <veysdoganer@gmail.com>
2023-01-07 18:12:40 +0300

    Week 17

---
+++ week04_10_01_2022/Phone.java
@@ -0,0 +1,27 @@
+package week04_10_01_2022;
+
+
+public class Phone{
+    static int number;
+
+    public Phone(String str){
+        this(str.length());
+    }
+    public Phone(int num){
+        this();
+        number +=num;
+    }
+
+    public Phone(){
+        number +=1;
+    }
+}
+
+class CallPhones {
+    public static void main(String[] args) {
+        new Phone("iphone");
+        new Phone();
+        new Phone(4);
+        System.out.println(Phone.number);
+    }
+}
---
+++ week_18_01_07_2023/ThrowsExample.java
@@ -0,0 +1,7 @@
+package week_18_01_07_2023;
+
+public class ThrowsExample {
+    public static void main(String[] args) {
+
+    }
+}
