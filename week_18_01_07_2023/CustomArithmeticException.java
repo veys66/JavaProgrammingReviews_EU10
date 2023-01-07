2fc7b519c08a267afe618f966a527a3ba0e0e0b4
veys66 <veysdoganer@gmail.com>
2023-01-07 18:39:56 +0300

    Week 17

--- week_18_01_07_2023/CustomArithmeticException.java
+++ week_18_01_07_2023/CustomArithmeticException.java
@@ -2,5 +2,7 @@ package week_18_01_07_2023;
 
 public class CustomArithmeticException extends RuntimeException{
     public CustomArithmeticException() {
+      super("You have an error with divide with 0");
+
     }
 }
