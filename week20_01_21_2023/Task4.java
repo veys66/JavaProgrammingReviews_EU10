67fb7a0cfb5bcb49f805f93cdd67bd7af2d269a0
veys66 <veysdoganer@gmail.com>
2023-01-28 18:34:12 +0300

    week20_01_21_2023

---
+++ week20_01_21_2023/Task4.java
@@ -0,0 +1,21 @@
+package week20_01_21_2023;
+
+public class Task4 {
+
+
+    /*
+ For compiler one of the most important task is balanced brackets
+            * Create a method that will accept a String of brackets. Determine if the brackets are balanced.
+            Brackets are balanced if the closing bracket matches the opening one.
+
+            Ex: {([])} -> balanced
+                {([])()} -> balanced
+                {[]([])()} -> balanced
+                {[(])} -> not balanced
+
+ */
+
+    public static void main(String[] args) {
+
+    }
+}
