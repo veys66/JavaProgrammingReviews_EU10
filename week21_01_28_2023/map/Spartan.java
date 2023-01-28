78d63e4301252c4118016486adb549188037037a
veys66 <veysdoganer@gmail.com>
2023-01-28 19:39:39 +0300

    week21_01_28_2023

---
+++ week21_01_28_2023/map/Spartan.java
@@ -0,0 +1,33 @@
+package week21_01_28_2023.map;
+
+public class Spartan {
+
+    String id;
+    String name;
+    String gender;
+
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+}
